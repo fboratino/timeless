096e18ac9be8e8b92e20fb0b914b342a68a601e4
Fernando Leite <fbleite@uolinc.com>
2020-06-11 10:13:03 -0300

    adjust in main.go

--- main.go
+++ main.go
@@ -9,16 +9,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@fotok.com\">support@fotok.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
+// GetPort defines a port for wild environment
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+		fmt.Println("INFO: No PORT environment detected", port)
 	}
+	return ":" + port
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -45,14 +43,4 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-}
-
-// GetPort defines a port for wild environment
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-		fmt.Println("INFO: No PORT environment detected", port)
-	}
-	return ":" + port
 }
